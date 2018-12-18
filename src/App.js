ca4fd87c9587b723530d9dc18689003bb98918e3
Ian <iangwaigdev@gmail.com>
2018-12-18 17:11:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> issa new feature.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://somethingincltd.com"
             target="_blank"
             rel="noopener noreferrer"
           >
